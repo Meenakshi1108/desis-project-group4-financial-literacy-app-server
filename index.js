918fd70b6e5b1be78b5da3ca5ba48d1b1c796f3e
Meenakshi S <meenakshisundar1108@gmail.com>
2024-03-22 17:21:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+import express from "express";
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+//components
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/blog", Router);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+Connection(username, password);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+    
+    app.get("/api/topics", async (req, res) => {
+      try {
+        const database = client.db("desisdb");
+        const collection = database.collection("topics");
+        const topics = await collection.find({}).toArray();
+        res.json(topics);
+      } catch (err) {
+        console.error("Error fetching topics:", err);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    app.get('/api/data/:category', async (req, res) => {
+      try {
+        const database = client.db('desisdb');
+        const collection = database.collection('default');
+        const category = req.params.category;
+    
+        let projection = {};
+        projection[category] = 1;
+        projection['_id'] = 0;
+    
+        const data = await collection.find({}).project(projection).toArray();
+    
+        console.log('Fetched data from MongoDB:', data);
+        if (!data || data.length === 0) {
+          return res.status(404).json({ message: 'No data found' });
+        }
+        res.json(data);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+    app.get("/api/resources", async (req, res) => {
+      try {
+        const database = client.db("desisdb");
+        const collection = database.collection("resources");
+
+        const documents = await collection.find({}).toArray();
+
+        console.log("Fetched data from MongoDB:", documents);
+
+        if (!documents || documents.length === 0) {
+          return res.status(404).json({ message: "No data found" });
+        }
+
+        // Transforming documents to match the desired structure
+        const transformedData = documents.reduce((acc, document) => {
+          Object.keys(document).forEach((key) => {
+            if (key !== "_id") {
+              // Skip _id field of the document
+              if (!acc[key]) {
+                acc[key] = [];
+              }
+              document[key].forEach((item) => {
+                const { id, ...rest } = item; // Destructure to omit id
+                acc[key].push(rest);
+              });
+            }
+          });
+          return acc;
+        }, {});
+
+        res.json(transformedData);
+      } catch (err) {
+        console.error("Error fetching data from resources collection:", err);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
