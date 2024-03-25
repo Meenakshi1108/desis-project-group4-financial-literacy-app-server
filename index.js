1f818c1220c62072365fa56245d43575ba71e928
Meenakshi S <meenakshisundar1108@gmail.com>
2024-03-25 15:11:20 +0530

    added comments

--- index.js
+++ index.js
@@ -3,21 +3,27 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-//components
+
+// Importing components
 import Connection from "./database/db.js";
 import Router from "./routes/route.js";
 
 dotenv.config();
 
+/**
+ * Initialize Express application
+ */
 const app = express();
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/blog", Router);
 
+// MongoDB client
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -29,13 +35,18 @@ const client = new MongoClient(uri, {
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
+// Establish MongoDB connection
 Connection(username, password);
 
+/**
+ * Main function to run the Express server
+ */
 async function run() {
   try {
     await client.connect();
     console.log("Connected to MongoDB!");
-    
+
+    // Route to fetch topics
     app.get("/api/topics", async (req, res) => {
       try {
         const database = client.db("desisdb");
@@ -48,18 +59,19 @@ async function run() {
       }
     });
 
+    // Route to fetch data by category
     app.get('/api/data/:category', async (req, res) => {
       try {
         const database = client.db('desisdb');
         const collection = database.collection('default');
         const category = req.params.category;
-    
+
         let projection = {};
         projection[category] = 1;
         projection['_id'] = 0;
-    
+
         const data = await collection.find({}).project(projection).toArray();
-    
+
         console.log('Fetched data from MongoDB:', data);
         if (!data || data.length === 0) {
           return res.status(404).json({ message: 'No data found' });
@@ -71,6 +83,7 @@ async function run() {
       }
     });
 
+    // Route to fetch resources data
     app.get("/api/resources", async (req, res) => {
       try {
         const database = client.db("desisdb");
@@ -113,8 +126,10 @@ async function run() {
   }
 }
 
+// Run the Express server
 run().catch(console.dir);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
